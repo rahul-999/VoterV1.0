134ca0cecc9a42e418b39e0a7605b5112ac41d42
Rahul Kachhadiya <rahul.kachhadiya99917@gmail.com>
2017-03-05 16:47:59 +0530

    The first page is created, which lists all the servers from available servers on LAN.  Still Login setup is REMAINING

--- app/src/main/java/com/example/admin/voterv10/MainActivity.java
+++ app/src/main/java/com/example/admin/voterv10/MainActivity.java
@@ -5,11 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declarations
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> arrayList;
+    ListView lview;
+    int flag=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initialize();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +44,66 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initialize() {
+
+        lview = (ListView)findViewById(R.id.listServers);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,arrayList);
+        lview.setAdapter(arrayAdapter);
+        addServer("Servers : ");
+        listSevers();
+        Log.d("Status","after call");
+    }
+
+    private void listSevers() {
+        final boolean[] proceed = {false};
+
+        final DatagramSocket[] ss = {null};
+        Thread thread= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Log.d("Socket","creating server socket...");
+                    ss[0] = new DatagramSocket(2566);
+                    proceed[0] =true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("Failiure","ServerSocket creation failed...");
+                }
+                while(true){
+                    if(proceed[0]){
+                        Log.d("STATUS","server socket is functionning...");
+                        try {
+                            byte []b = new byte[5];
+                            final DatagramPacket dp = new DatagramPacket(b,5);
+                            ss[0].receive(dp);
+                            Log.d("Status","socket is createad successfully...");
+                            Log.d("Message received", new String(dp.getData(),"UTF-8"));
+                            Log.d("IP",dp.getAddress().getHostName());
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    addServer(dp.getAddress().getHostName());
+                                }
+                            });
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Log.d("STATIS","Listener Socket creation failed...");
+                        }
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+
+
+    private void addServer(String str) {
+        arrayAdapter.add(str);
+        arrayAdapter.notifyDataSetChanged();
     }
 
     @Override
